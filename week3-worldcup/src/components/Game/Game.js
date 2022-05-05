bbcde4f2c1a2658d4f2ef838c06bb3aa9c07f302
Chedda98 <twa05157@gmail.com>
2022-05-06 02:35:21 +0900

    Feat: 이상형 월드컵 style 설정

--- week3-worldcup/src/components/Game/Game.js
+++ week3-worldcup/src/components/Game/Game.js
@@ -3,11 +3,14 @@ import { FlexBox } from './Style';
 
 const Game = () => {
   return (<FlexBox>
+    <h1 className="title">이 상 형 월 드 컵</h1>
     <div className="flex-1">
       <img className="handsome-img" src={require("../../Img/민현.jpeg")} />
+      <div className="name">황민현</div>
     </div>
     <div className="flex-1">
       <img className="handsome-img" src={require("../../Img/효섭.jpeg")} />
+      <div className="name">안효섭</div>
     </div>
   </FlexBox>
   );
--- week3-worldcup/src/components/Game/Style.js
+++ week3-worldcup/src/components/Game/Style.js
@@ -5,13 +5,45 @@ export const FlexBox = styled.div`
   flex-wrap: wrap;
   height: 100vh;
 
+  .title {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #fff;
+    padding: 0px 30px;
+    text-transform: uppercase;
+    padding-bottom: 10px;
+  }
+
   .flex-1 {
     flex: 1;
     min-width: 500px;
+    overflow: hidden;
+    background-color: black;
+    position: relative;
   }
 
   .handsome-img {
     width: 100%;
     height: 100%;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+  
+  .handsome-img: hover {
+    transform: scale(1.1);
+    opacity: 0.8;
+  }
+
+  .name {
+    position: absolute;
+    z-index: 3;
+    color: #000;
+    bottom: 10%;
+    font-size: 90px;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
