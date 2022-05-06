e0d5f02b1eb1d379bfd6365f167d42689b8ac65d
Chedda98 <twa05157@gmail.com>
2022-05-06 12:41:25 +0900

    Fix: 결승전 진출 구현

--- week3-worldcup/src/components/Game/Game.js
+++ week3-worldcup/src/components/Game/Game.js
@@ -31,7 +31,7 @@ const Game = () => {
     setDisplays([items[0], items[1]]);
   }, []);
 
-  const clickHandler = handsome => () => { // handsome 객체가 들어갔을 때 반환되는 이벤트를 다시 parameter로 받아서 반환해야하기 때문에 감쌈
+  const clickHandler = handsome => (event) => { // handsome 객체가 들어갔을 때 반환되는 이벤트를 다시 parameter로 받아서 반환해야하기 때문에 감쌈
     if (handsomes.length <= 2) {
       if (winners.length === 0) { // 결승전
         setDisplays([handsome]);
@@ -41,20 +41,21 @@ const Game = () => {
         setDisplays([updatedHandsome[0], updatedHandsome[1]]);
         setWinners([]);
       }
-    } else if (handsomes.length > 2) // 남은 핸썸가이가 두명 이상이면 계속해서 이상형 월드컵 진행
+    } else if (handsomes.length > 2) { // 남은 핸썸가이가 두명 이상이면 계속해서 이상형 월드컵 진행
       setWinners([...winners, handsome])
       setDisplays([handsomes[2], handsomes[3]])
       setHansomes(handsomes.slice(2)) // 현재 노출된 2개 제외
-  }
+    }
+  };
 
   return (
     <FlexBox>
       <h1 className="title">이 상 형 월 드 컵</h1>
-      {displays.map(d => { // map을 사용할 때는 key 값을 반드시 설정해줘야함!
+      {displays.map(item => { // map을 사용할 때는 key 값을 반드시 설정해줘야함!
         return (
-          <div className="flex-1" key={d.name} onClick={clickHandler(d)}>
-            <img className="handsome-img" src={d.src} />
-            <div className="name">{d.name}</div>
+          <div className="flex-1" key={item.name} onClick={clickHandler(item)}>
+            <img className="handsome-img" src={item.src} alt="handsome" />
+            <div className="name">{item.name}</div>
           </div>
         );
       })}
